d7a2a03a0966b35a0ee9fb089b1f892eeaa57b1c
DunneDev <dunn0203@algonquinlive.com>
2023-04-07 18:03:21 -0400

    made changes

--- routes.js
+++ routes.js
@@ -28,8 +28,7 @@ function checkUserNotInGame(req, res, next) {
                     res.redirect("/");
                 } else {
                     if (result.length > 0) {
-                        console.log("User is in a game");
-                        res.redirect(`/game`);
+                        res.redirect(`/game/${result[0].id}`);
                     } else {
                         console.log("User is not in a game");
                         next();
@@ -51,7 +50,6 @@ function checkUserInGame(req, res, next) {
                     res.redirect("/");
                 } else {
                     if (result.length > 0) {
-                        console.log("User is in a game");
                         next();
                     } else {
                         console.log("User is not in a game");
@@ -70,7 +68,10 @@ module.exports = (app) => {
             `SELECT games.id, lobby_name, u1.username AS host_username FROM games
                 LEFT JOIN (SELECT id, username FROM users) u1 ON games.player1_userid = u1.id
                 LEFT JOIN (SELECT id, username FROM users) u2 ON games.player2_userid = u2.id
-            WHERE is_active = TRUE`,
+            WHERE is_active = TRUE
+            AND player1_userid IS NOT NULL
+            AND player2_userid IS NULL
+            `,
             (err, result) => {
                 if (err) {
                     console.log(err);
@@ -143,8 +144,57 @@ module.exports = (app) => {
         );
     });
 
-    app.get("/game", checkUserInGame, (req, res) => {
-        res.send("Game");
+    app.get("/game/:GameId", checkUserInGame, (req, res) => {
+        const gameId = req.params.GameId;
+        const userId = req.user.id;
+
+        // Get the game from the database
+        db.query(
+            `SELECT games.id, lobby_name, u1.username AS host_username,
+            u2.username AS guest_username, player1_userid, player2_userid
+            FROM games
+                LEFT JOIN (SELECT id, username FROM users) u1 ON games.player1_userid = u1.id
+                LEFT JOIN (SELECT id, username FROM users) u2 ON games.player2_userid = u2.id
+            WHERE games.id = ?`,
+            [gameId],
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/err");
+                } else {
+                    if (
+                        userId === result[0].player1_userid ||
+                        userId === result[0].player2_userid
+                    ) {
+                        // Render the game page with the game
+                        res.render("game", { game: result[0], user: req.user });
+                    }
+                    // redirect to home page if user is not in the game
+                    else {
+                        res.redirect("/");
+                    }
+                }
+            }
+        );
+    });
+
+    app.get("/game/:gameId/join", checkUserNotInGame, (req, res) => {
+        const gameId = req.params.gameId;
+        const player2_userid = req.user.id;
+
+        // modify the game in the database
+        db.query(
+            "UPDATE games SET player2_userid = ? WHERE id = ?",
+            [player2_userid, gameId],
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/err");
+                } else {
+                    res.redirect(`/game/${gameId}`);
+                }
+            }
+        );
     });
 
     app.post("/game/new", (req, res) => {
